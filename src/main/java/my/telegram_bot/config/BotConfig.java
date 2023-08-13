4ad2d9c6d5a72f89d6473dbe454b65ac44c5e9d8
Yaroslav Agapov <euraks@yandex.ru>
2023-08-13 09:05:50 +0500

    Base Config Complete

---
+++ src/main/java/my/telegram_bot/config/BotConfig.java
@@ -0,0 +1,18 @@
+package my.telegram_bot.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Data
+@Configuration
+@PropertySource( "application.properties" )
+public class BotConfig {
+
+    @Value( "${bot.name}" )
+    String botName;
+
+    @Value( "${bot.key}" )
+    String token;
+}
